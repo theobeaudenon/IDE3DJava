47af83bd3c3604213bc335949996ed151459ae25
Theo Beaudenon <theobeaudenon@yahoo.fr>
2015-01-06 09:14:27 +0100

    bolasoue!

---
+++ src/classe/BoLASoupe.java
@@ -0,0 +1,45 @@
+package classe;
+
+/**
+ * classe
+ * Created by Theo on 06/01/2015 for Ide3DProject.
+ */
+public class BoLASoupe {
+
+    Forme forme;
+    Float x;
+    Float y;
+    Float z;
+
+    public Forme getForme() {
+        return forme;
+    }
+
+    public void setForme(Forme forme) {
+        this.forme = forme;
+    }
+
+    public Float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Float getZ() {
+        return z;
+    }
+
+    public void setZ(Float z) {
+        this.z = z;
+    }
+}
