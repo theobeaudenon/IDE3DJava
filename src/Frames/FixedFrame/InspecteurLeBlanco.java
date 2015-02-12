17e7d2d5b2898c02c6c036e0fab5d5d1f0db5299
Your Name <you@example.com>
2015-02-12 11:01:20 +0100

    zgrgfsgfd

--- src/Frames/FixedFrame/InspecteurLeBlanco.java
+++ src/Frames/FixedFrame/InspecteurLeBlanco.java
@@ -5,6 +5,7 @@ import Frames.Dialog.PopupObjectFrame;
 import Frames.InternalFrameDemo;
 import buttons.ColorButton;
 import classe.*;
+import utils.CustomTextField;
 import utils.Menu.RightClicMenu;
 
 import javax.swing.*;
@@ -26,6 +27,16 @@ public class InspecteurLeBlanco extends JInternalFrame {
     private JPanel top = new JPanel();
     private RightClicMenu rightClicMenu = new RightClicMenu();
     private InternalFrameDemo parent;
+    private JLabel spawnLable = new JLabel("SpawnPoint : ");
+    private CustomTextField xObject = new CustomTextField(0);
+    private CustomTextField yObject = new CustomTextField(0);
+    private CustomTextField zObject = new CustomTextField(0);
+    private JLabel x = new JLabel("x");
+    private JLabel y = new JLabel("y");
+    private JLabel z = new JLabel("z");
+    private JScrollPane jScrollPane = new JScrollPane();
+
+
 
     public InspecteurLeBlanco(final InternalFrameDemo parent) {
         super("Project",
@@ -122,12 +133,14 @@ public class InspecteurLeBlanco extends JInternalFrame {
     }
 
     public void setModule(final Scene module) {
-        top.setLayout(new GridLayout(1, 2, 10, 10));
+        top.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         top.removeAll();
         top.repaint();
         top.setPreferredSize(this.getSize());
 
         final JTextField name = new JTextField(module.getName());
+        name.setPreferredSize(new Dimension(300, 35));
         name.setBackground(new Color(45, 48, 50));
         name.setForeground(Color.WHITE);
         name.setHorizontalAlignment(SwingConstants.CENTER);
@@ -153,7 +166,11 @@ public class InspecteurLeBlanco extends JInternalFrame {
                 parent.refreshTree();
             }
         });
-        top.add(name);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        top.add(name,c);
         final DefaultListModel model = new DefaultListModel();
         for (final BoLASoupe forme : module.getFormes()) {
             model.addElement(forme);
@@ -161,7 +178,7 @@ public class InspecteurLeBlanco extends JInternalFrame {
         }
         final JList list = new JList(model);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.setAutoscrolls(true);
+        //list.setAutoscrolls(true);
         list.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
         list.setBackground(new Color(60, 63, 65));
         list.setForeground(new Color(205, 198, 183));
@@ -175,14 +192,14 @@ public class InspecteurLeBlanco extends JInternalFrame {
             public void mouseReleased(MouseEvent e) {
                 for (final BoLASoupe forme : module.getFormes()) {
                     forme.setSelected(false);
-                 }
+                }
                 if (e.getClickCount() == 2 && !e.isConsumed()) {
                     e.consume();
                     int index = list.locationToIndex(e.getPoint());
                     BoLASoupe item = (BoLASoupe) model.getElementAt(index);
 
-                    PopupObjectAdd ef = new PopupObjectAdd(parent , item);
-                    if(ef.getBol() != null){
+                    PopupObjectAdd ef = new PopupObjectAdd(parent, item);
+                    if (ef.getBol() != null) {
                         //d.addForme(ef.getBol());
                         // parent.log("Ajouté : " + comboBox.getSelectedItem());
                         //parent.updateInspecteur(d);
@@ -192,17 +209,170 @@ public class InspecteurLeBlanco extends JInternalFrame {
                 }
             }
         });
-        top.add(list);
+        jScrollPane =  new JScrollPane(list);
+        jScrollPane.setBorder(BorderFactory.createEmptyBorder());
+        jScrollPane.setPreferredSize(new Dimension(300,200));
+
+        x.setForeground(Color.WHITE);
+        x.setFont(new Font("Courier", Font.ITALIC, 21));
+        y.setForeground(Color.WHITE);
+        y.setFont(new Font("Courier", Font.ITALIC, 21));
+        z.setForeground(Color.WHITE);
+        z.setFont(new Font("Courier", Font.ITALIC, 21));
+        spawnLable.setForeground(Color.WHITE);
+        spawnLable.setFont(new Font("Courier", Font.ITALIC, 21));
+
+        xObject.setPreferredSize(new Dimension(40, 20));
+        xObject.setText(String.valueOf(module.getSpawnX()));
+        xObject.setBackground(new Color(45, 48, 50));
+        xObject.setForeground(new Color(145, 145, 145));
+        xObject.setHorizontalAlignment(SwingConstants.CENTER);
+        xObject.setCaretColor(Color.WHITE);
+        xObject.setBorder(BorderFactory.createEmptyBorder());
+        xObject.setFont(new Font("Courier", Font.ITALIC, 21));
+        xObject.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void warn() {
+                try{
+                    module.setSpawnX(Float.parseFloat(xObject.getText()));
+                }
+                catch (NumberFormatException e){
+                    module.setSpawnX(0);
+                }
+            }
+        });
+
+        yObject.setPreferredSize(new Dimension(40, 20));
+        yObject.setText(String.valueOf(module.getSpawnY()));
+        yObject.setBackground(new Color(45, 48, 50));
+        yObject.setForeground(new Color(145, 145, 145));
+        yObject.setHorizontalAlignment(SwingConstants.CENTER);
+        yObject.setCaretColor(Color.WHITE);
+        yObject.setBorder(BorderFactory.createEmptyBorder());
+        yObject.setFont(new Font("Courier", Font.ITALIC, 21));
+        yObject.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void warn() {
+                try{
+                    module.setSpawnY(Float.parseFloat(yObject.getText()));
+                }
+                catch (NumberFormatException e){
+                    module.setSpawnY(0);
+                }
+            }
+        });
+
+
+        zObject.setPreferredSize(new Dimension(40,20));
+        zObject.setText(String.valueOf(module.getSpawnZ()));
+        zObject.setBackground(new Color(45, 48, 50));
+        zObject.setForeground(new Color(145, 145, 145));
+        zObject.setHorizontalAlignment(SwingConstants.CENTER);
+        zObject.setCaretColor(Color.WHITE);
+        zObject.setBorder(BorderFactory.createEmptyBorder());
+        zObject.setFont(new Font("Courier", Font.ITALIC, 21));
+        zObject.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void warn() {
+                try{
+                    module.setSpawnZ(Float.parseFloat(zObject.getText()));
+                }
+                catch (NumberFormatException e){
+                    module.setSpawnZ(0);
+                }
+            }
+        });
+
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        top.add(jScrollPane, c);
+        jScrollPane.repaint();
+        top.repaint();
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.insets = new Insets(0,2,0,0);
+        top.add(spawnLable,c);
+
+        c.gridx = 1;
+        c.gridy = 2;
+        top.add(x,c);
+
+        c.gridx = 2;
+        c.gridy = 2;
+        top.add(xObject,c);
+
+        c.gridx = 3;
+        c.gridy = 2;
+        top.add(y,c);
+
+        c.gridx = 4;
+        c.gridy = 2;
+        top.add(yObject,c);
+
+        c.gridx = 5;
+        c.gridy = 2;
+        top.add(z,c);
+
+        c.gridx = 6;
+        c.gridy = 2;
+        top.add(zObject,c);
 
         JButton bt = new JButton("PLAY");
+        bt.setPreferredSize(new Dimension(100,35));
+        bt.setBorder(BorderFactory.createBevelBorder(1,Color.black,Color.cyan));
+        bt.setForeground(Color.WHITE);
+        bt.setBackground(new Color(46,46,46));
+        bt.setSelected(false);
         bt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 parent.playgame(module);
             }
         });
 
-        top.add(bt);
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(20,0,0,0);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        top.add(bt,c);
 
     }
 
